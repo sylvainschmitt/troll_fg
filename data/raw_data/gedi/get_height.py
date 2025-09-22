f87fddc7c9037bee0f57c0d4808f3743e75fde69
Sylvain Schmitt <sylvain.m.schmitt@gmail.com>
2025-09-22 10:35:17 +0200

    troll_fg 0.1.0.9005: test results guyafor & guyaflux

--- data/raw_data/gedi/get_height.py
+++ data/raw_data/gedi/get_height.py
@@ -6,9 +6,10 @@ import xarray as xr
 import ee
 
 ee.Initialize(project="ee-sylvainmschmitt", opt_url='https://earthengine-highvolume.googleapis.com')
-ic = ee.ImageCollection(ee.Image("projects/sat-io/open-datasets/CTREES/AMAZON-CANOPY-TREE-HT"))
+ic = ee.ImageCollection("LARSE/GEDI/GEDI02_A_002_MONTHLY")
 leg = ee.Geometry.Rectangle([-53, 4, -52, 5], proj="EPSG:4326")
-ds = xr.open_mfdataset([ic], engine='ee', projection="EPSG:4326", scale = 0.0001, geometry=leg)
+ds = xr.open_mfdataset([ic], engine='ee', projection="EPSG:4326", scale = 0.1, geometry=leg, fast_time_slicing=True)
+ds = ds[['rh98']]
 ds.to_netcdf('canopy_height.nc')
 
 # csv transfer
